b1ad01378b1e97d5599c3888cd37bb5760724d33
Jonathan Olson <jonathan.olson@colorado.edu>
2022-08-15 20:36:41 -0600

    Grunt update, and changes for string Properties, see https://github.com/phetsims/chipper/issues/1302

--- js/buoyancyStrings.ts
+++ js/buoyancyStrings.ts
@@ -5,17 +5,23 @@
  */
 /* eslint-disable */
 import getStringModule from '../../chipper/js/getStringModule.js';
+import TReadOnlyProperty from '../../axon/js/TReadOnlyProperty.js';
 import buoyancy from './buoyancy.js';
 
 type StringsType = {
   'buoyancy': {
     'title': string;
+    'titleProperty': TReadOnlyProperty<string>;
   };
   'screen': {
     'intro': string;
+    'introProperty': TReadOnlyProperty<string>;
     'explore': string;
+    'exploreProperty': TReadOnlyProperty<string>;
     'shapes': string;
+    'shapesProperty': TReadOnlyProperty<string>;
     'applications': string;
+    'applicationsProperty': TReadOnlyProperty<string>;
   }
 };
 
