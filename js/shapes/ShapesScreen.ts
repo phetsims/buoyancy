f65ba2f7265fae9110ffd67139351367e83f3e2e
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-23 16:06:52 -0600

    add shapes screen icon, https://github.com/phetsims/buoyancy/issues/48
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/shapes/ShapesScreen.ts
+++ js/shapes/ShapesScreen.ts
@@ -13,9 +13,14 @@ import Screen from '../../../joist/js/Screen.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import buoyancy from '../buoyancy.js';
 import BuoyancyStrings from '../BuoyancyStrings.js';
+import DensityBuoyancyScreenView from '../../../density-buoyancy-common/js/common/view/DensityBuoyancyScreenView.js';
+import shapes_screen_icon_png from '../../mipmaps/shapes_screen_icon_png.js';
+import ScreenIcon from '../../../joist/js/ScreenIcon.js';
 
 export default class ShapesScreen extends Screen<BuoyancyShapesModel, BuoyancyShapesScreenView> {
   public constructor( tandem: Tandem ) {
+    const icon = DensityBuoyancyScreenView.getThreeIcon( shapes_screen_icon_png, () => BuoyancyShapesScreenView.getBuoyancyShapesIcon() );
+
     super(
       () => new BuoyancyShapesModel( {
         tandem: tandem.createTandem( 'model' )
@@ -26,6 +31,10 @@ export default class ShapesScreen extends Screen<BuoyancyShapesModel, BuoyancySh
       {
         name: BuoyancyStrings.screen.shapesStringProperty,
         backgroundColorProperty: DensityBuoyancyCommonColors.skyBottomProperty,
+        homeScreenIcon: new ScreenIcon( icon, {
+          maxIconWidthProportion: 1,
+          maxIconHeightProportion: 1
+        } ),
         tandem: tandem
       }
     );
