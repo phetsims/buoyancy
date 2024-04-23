ae9c6154c98522355862456610d13aef47ca39e0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-23 16:08:38 -0600

    add placeholder icons, https://github.com/phetsims/buoyancy/issues/48
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/applications/ApplicationsScreen.ts
+++ js/applications/ApplicationsScreen.ts
@@ -13,9 +13,14 @@ import Screen from '../../../joist/js/Screen.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import buoyancy from '../buoyancy.js';
 import BuoyancyStrings from '../BuoyancyStrings.js';
+import DensityBuoyancyScreenView from '../../../density-buoyancy-common/js/common/view/DensityBuoyancyScreenView.js';
+import shapes_screen_icon_png from '../../mipmaps/shapes_screen_icon_png.js';
+import BuoyancyShapesScreenView from '../../../density-buoyancy-common/js/buoyancy/view/BuoyancyShapesScreenView.js';
+import ScreenIcon from '../../../joist/js/ScreenIcon.js';
 
 export default class ApplicationsScreen extends Screen<BuoyancyApplicationsModel, BuoyancyApplicationsScreenView> {
   public constructor( tandem: Tandem ) {
+    const icon = DensityBuoyancyScreenView.getThreeIcon( shapes_screen_icon_png, () => BuoyancyShapesScreenView.getBuoyancyShapesIcon() );
     super(
       () => new BuoyancyApplicationsModel( {
         tandem: tandem.createTandem( 'model' )
@@ -26,6 +31,10 @@ export default class ApplicationsScreen extends Screen<BuoyancyApplicationsModel
       {
         name: BuoyancyStrings.screen.applicationsStringProperty,
         backgroundColorProperty: DensityBuoyancyCommonColors.skyBottomProperty,
+        homeScreenIcon: new ScreenIcon( icon, {
+          maxIconWidthProportion: 1,
+          maxIconHeightProportion: 1
+        } ),
         tandem: tandem
       }
     );
--- js/explore/ExploreScreen.ts
+++ js/explore/ExploreScreen.ts
@@ -13,9 +13,14 @@ import Screen from '../../../joist/js/Screen.js';
 import Tandem from '../../../tandem/js/Tandem.js';
 import buoyancy from '../buoyancy.js';
 import BuoyancyStrings from '../BuoyancyStrings.js';
+import ScreenIcon from '../../../joist/js/ScreenIcon.js';
+import DensityBuoyancyScreenView from '../../../density-buoyancy-common/js/common/view/DensityBuoyancyScreenView.js';
+import shapes_screen_icon_png from '../../mipmaps/shapes_screen_icon_png.js';
+import BuoyancyShapesScreenView from '../../../density-buoyancy-common/js/buoyancy/view/BuoyancyShapesScreenView.js';
 
 export default class ExploreScreen extends Screen<BuoyancyExploreModel, BuoyancyExploreScreenView> {
   public constructor( tandem: Tandem ) {
+    const icon = DensityBuoyancyScreenView.getThreeIcon( shapes_screen_icon_png, () => BuoyancyShapesScreenView.getBuoyancyShapesIcon() );
     super(
       () => new BuoyancyExploreModel( {
         tandem: tandem.createTandem( 'model' )
@@ -26,6 +31,10 @@ export default class ExploreScreen extends Screen<BuoyancyExploreModel, Buoyancy
       {
         name: BuoyancyStrings.screen.exploreStringProperty,
         backgroundColorProperty: DensityBuoyancyCommonColors.skyBottomProperty,
+        homeScreenIcon: new ScreenIcon( icon, {
+          maxIconWidthProportion: 1,
+          maxIconHeightProportion: 1
+        } ),
         tandem: tandem
       }
     );
