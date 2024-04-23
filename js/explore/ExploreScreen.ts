2912013306bd7381897247f12559f663e08ca8e0
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-04-23 16:34:16 -0600

    add buoyancy explore screen icon, https://github.com/phetsims/buoyancy/issues/48
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/explore/ExploreScreen.ts
+++ js/explore/ExploreScreen.ts
@@ -14,13 +14,10 @@ import Tandem from '../../../tandem/js/Tandem.js';
 import buoyancy from '../buoyancy.js';
 import BuoyancyStrings from '../BuoyancyStrings.js';
 import ScreenIcon from '../../../joist/js/ScreenIcon.js';
-import DensityBuoyancyScreenView from '../../../density-buoyancy-common/js/common/view/DensityBuoyancyScreenView.js';
-import shapes_screen_icon_png from '../../mipmaps/shapes_screen_icon_png.js';
-import BuoyancyShapesScreenView from '../../../density-buoyancy-common/js/buoyancy/view/BuoyancyShapesScreenView.js';
 
 export default class ExploreScreen extends Screen<BuoyancyExploreModel, BuoyancyExploreScreenView> {
   public constructor( tandem: Tandem ) {
-    const icon = DensityBuoyancyScreenView.getThreeIcon( shapes_screen_icon_png, () => BuoyancyShapesScreenView.getBuoyancyShapesIcon() );
+    const icon = BuoyancyExploreScreenView.getBuoyancyExploreIcon();
     super(
       () => new BuoyancyExploreModel( {
         tandem: tandem.createTandem( 'model' )
