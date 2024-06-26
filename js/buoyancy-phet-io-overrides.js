1d1fe099cf11749d272c71a172c8b8cf7c13c872
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-06-26 15:13:08 -0600

    add doc via overrides, https://github.com/phetsims/density-buoyancy-common/issues/232
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -1,2 +1,7 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "buoyancy.applicationsScreen.model.objects.boat.volumeProperty": {
+      "phetioDocumentation": "Volume of the boat hull."
+    }
+  };
