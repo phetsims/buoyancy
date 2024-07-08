97875e14bdfff4a17dc83029ce5fe1d8e68d98e4
Michael Kauzmann <michael.kauzmann@colorado.edu>
2024-07-08 15:43:13 -0600

    update doc, https://github.com/phetsims/density-buoyancy-common/issues/245
    Signed-off-by: Michael Kauzmann <michael.kauzmann@colorado.edu>

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -3,5 +3,8 @@ window.phet.preloads.phetio.phetioElementsOverrides =
   {
     "buoyancy.applicationsScreen.model.objects.boat.volumeProperty": {
       "phetioDocumentation": "Volume of the boat hull."
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.volumeProperty": {
+      "phetioDocumentation": "Volume of the bottle."
     }
   };
