62654bf3affa138c8bff5a85655d66a9d45a1c63
Sam Reid <reids@colorado.edu>
2024-07-29 12:44:08 -0600

    Apply formatter, see https://github.com/phetsims/density-buoyancy-common/issues/123

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -1,10 +1,10 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
   {
-    "buoyancy.applicationsScreen.model.objects.boat.volumeProperty": {
-      "phetioDocumentation": "Volume of the boat hull."
+    'buoyancy.applicationsScreen.model.objects.boat.volumeProperty': {
+      'phetioDocumentation': 'Volume of the boat hull.'
     },
-    "buoyancy.applicationsScreen.model.objects.bottle.volumeProperty": {
-      "phetioDocumentation": "Volume of the bottle."
+    'buoyancy.applicationsScreen.model.objects.bottle.volumeProperty': {
+      'phetioDocumentation': 'Volume of the bottle.'
     }
   };
