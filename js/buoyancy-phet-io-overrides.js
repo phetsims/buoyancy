933ebadcc9484011a9fb842269af693cf438ff02
Sam Reid <reids@colorado.edu>
2024-08-07 10:13:48 -0600

    Move overrides into the code, see https://github.com/phetsims/phet-io/issues/1873

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -1,10 +1,2 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides =
-  {
-    "buoyancy.applicationsScreen.model.objects.boat.volumeProperty": {
-      "phetioDocumentation": "Volume of the boat hull."
-    },
-    "buoyancy.applicationsScreen.model.objects.bottle.volumeProperty": {
-      "phetioDocumentation": "Volume of the bottle."
-    }
-  };
+window.phet.preloads.phetio.phetioElementsOverrides = {};
