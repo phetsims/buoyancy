3dca902401c6e44ba0a6f0968b5df9cd0fbc61ba
Sam Reid <reids@colorado.edu>
2024-08-13 07:58:40 -0600

    Add phet-io-overrides for phetioFeatured, see https://github.com/phetsims/density-buoyancy-common/issues/325

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -1,2 +1,157 @@
 /* eslint-disable */
-window.phet.preloads.phetio.phetioElementsOverrides = {};
+window.phet.preloads.phetio.phetioElementsOverrides =
+  {
+    "buoyancy.applicationsScreen.model.applicationModeProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.boat.buoyancyForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.boat.contactForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.boat.gravityForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.boat.maxVolumeDisplacedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.boat.percentSubmergedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.boat.userControlledProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.buoyancyForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.contactForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.gravityForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.materialInside.massProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.materialInside.volumeProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.percentSubmergedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.model.objects.bottle.userControlledProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.view.blockControls.massNumberControl.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.view.blockControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.view.bottleControls.materialInsideControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.applicationsScreen.view.bottleControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.buoyancyForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.contactForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.gravityForceInterpolatedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.percentSubmergedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.userControlledProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.pool.fluid.levelVolumeProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.pool.scale.heightProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.model.pool.scale.percentSubmergedProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.blocksValuePanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayOptionsPanel.forcesSubpanel.vectorZoomButtonGroup.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayOptionsPanel.forcesSubpanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayOptionsPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayProperties.buoyancyForceVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayProperties.contactForceVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayProperties.depthLinesVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayProperties.forceValuesVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.displayProperties.gravityForceVisibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.fluidPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.compareScreen.view.percentSubmergedAccordionBox": {
+      "phetioFeatured": true
+    },
+    "buoyancy.exploreScreen.view.fluidDensityPanel.numberControl": {
+      "phetioFeatured": true
+    },
+    "buoyancy.exploreScreen.view.fluidDensityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.labScreen.model.gravityProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.labScreen.model.gravityProperty.customGravity.gravityValueProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.labScreen.view.blockPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.labScreen.view.fluidDisplacedAccordionBox": {
+      "phetioFeatured": true
+    },
+    "buoyancy.labScreen.view.gravityPanel.numberControl": {
+      "phetioFeatured": true
+    },
+    "buoyancy.labScreen.view.gravityPanel.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.shapesScreen.model.objects.objectA.heightRatioProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.shapesScreen.model.objects.objectA.shapeNameProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.shapesScreen.model.objects.objectA.shapeProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.shapesScreen.model.objects.objectA.widthRatioProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.shapesScreen.view.objectAShapeSizeControls.visibleProperty": {
+      "phetioFeatured": true
+    },
+    "buoyancy.shapesScreen.view.objectAShapeSizeControls.volumeDisplay.visibleProperty": {
+      "phetioFeatured": true
+    }
+  };
