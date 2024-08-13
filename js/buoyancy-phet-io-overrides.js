7b570b8291a97c6dd78adca3fc2867099bd7ed32
Sam Reid <reids@colorado.edu>
2024-08-13 08:11:38 -0600

    Move phet-io-overrides to code, see https://github.com/phetsims/density-buoyancy-common/issues/325

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -1,34 +1,10 @@
 /* eslint-disable */
 window.phet.preloads.phetio.phetioElementsOverrides =
   {
-    "buoyancy.applicationsScreen.model.objects.boat.maxVolumeDisplacedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.boat.percentSubmergedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.boat.userControlledProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.bottle.buoyancyForceInterpolatedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.bottle.contactForceInterpolatedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.bottle.gravityForceInterpolatedProperty": {
-      "phetioFeatured": true
-    },
     "buoyancy.applicationsScreen.model.objects.bottle.materialInside.massProperty": {
       "phetioFeatured": true
     },
     "buoyancy.applicationsScreen.model.objects.bottle.materialInside.volumeProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.bottle.percentSubmergedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.applicationsScreen.model.objects.bottle.userControlledProperty": {
       "phetioFeatured": true
     },
     "buoyancy.applicationsScreen.view.blockControls.massNumberControl.visibleProperty": {
@@ -43,28 +19,10 @@ window.phet.preloads.phetio.phetioElementsOverrides =
     "buoyancy.applicationsScreen.view.bottleControls.visibleProperty": {
       "phetioFeatured": true
     },
-    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.buoyancyForceInterpolatedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.contactForceInterpolatedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.gravityForceInterpolatedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.percentSubmergedProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.compareScreen.model.blockSets.sameDensity.block3A.userControlledProperty": {
-      "phetioFeatured": true
-    },
     "buoyancy.compareScreen.model.pool.fluid.levelVolumeProperty": {
       "phetioFeatured": true
     },
     "buoyancy.compareScreen.model.pool.scale.heightProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.compareScreen.model.pool.scale.percentSubmergedProperty": {
       "phetioFeatured": true
     },
     "buoyancy.compareScreen.view.blocksValuePanel.visibleProperty": {
