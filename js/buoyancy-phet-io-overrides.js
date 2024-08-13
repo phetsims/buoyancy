ba5b41bc1ee9558cb219b96b82292649d704666c
Sam Reid <reids@colorado.edu>
2024-08-13 08:46:10 -0600

    Move phet-io-overrides to code, see https://github.com/phetsims/density-buoyancy-common/issues/325

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -31,9 +31,6 @@ window.phet.preloads.phetio.phetioElementsOverrides =
     "buoyancy.labScreen.view.gravityPanel.visibleProperty": {
       "phetioFeatured": true
     },
-    "buoyancy.shapesScreen.view.objectAShapeSizeControls.visibleProperty": {
-      "phetioFeatured": true
-    },
     "buoyancy.shapesScreen.view.objectAShapeSizeControls.volumeDisplay.visibleProperty": {
       "phetioFeatured": true
     }
