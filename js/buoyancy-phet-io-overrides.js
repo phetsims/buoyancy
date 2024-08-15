c256ecce9f4bc7f2b3139432ff84b245391bb5e6
Sam Reid <reids@colorado.edu>
2024-08-15 08:51:49 -0600

    Move phet-io-overrides to code, see https://github.com/phetsims/density-buoyancy-common/issues/325

--- js/buoyancy-phet-io-overrides.js
+++ js/buoyancy-phet-io-overrides.js
@@ -7,28 +7,7 @@ window.phet.preloads.phetio.phetioElementsOverrides =
     "buoyancy.general.view.navigationBar.preferencesButton.preferencesDialogCapsule.archetype.preferencesPanels.simulationPreferencesPanel.percentSubmergedVisibleControl.visibleProperty": {
       "phetioFeatured": true
     },
-    "buoyancy.general.view.navigationBar.preferencesButton.preferencesDialogCapsule.archetype.preferencesPanels.simulationPreferencesPanel.volumeUnitsControl": {
-      "phetioFeatured": true
-    },
     "buoyancy.general.view.navigationBar.preferencesButton.preferencesDialogCapsule.archetype.preferencesPanels.simulationPreferencesPanel.volumeUnitsControl.visibleProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.global.view.colors.materialTColorProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.global.view.colors.materialUColorProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.global.view.colors.materialVColorProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.global.view.colors.materialWColorProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.global.view.colors.materialXColorProperty": {
-      "phetioFeatured": true
-    },
-    "buoyancy.global.view.colors.materialYColorProperty": {
       "phetioFeatured": true
     }
   };
